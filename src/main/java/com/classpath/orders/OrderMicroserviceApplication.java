dd6068bd6d335d00a21d5ab8b4461d1830b3ea08
Karthik Hebbar <9211karthik.h@gmail.com>
2022-10-13 12:55:24 +0530

    Auto create topic is set to false

--- src/main/java/com/classpath/orders/OrderMicroserviceApplication.java
+++ src/main/java/com/classpath/orders/OrderMicroserviceApplication.java
@@ -3,12 +3,11 @@ package com.classpath.orders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
 
 import com.classpath.orders.channel.OutputChannel;
 
 @SpringBootApplication
-@EnableBinding({Source.class, OutputChannel.class})
+@EnableBinding(OutputChannel.class)
 public class OrderMicroserviceApplication {
 
 	public static void main(String[] args) {
